82c2814cb740a9f9d97bd9caec1858a6966ce545
Namash <namashaggarwal@gmail.com>
2021-05-17 00:54:47 -0700

    1.0.0 version

---
+++ 01.py
@@ -0,0 +1,27 @@
+class TimeTravelReomote():
+    
+
+
+    def __init__(self):
+
+        self.remote = ["Past","Present","Future"]
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+         
+         self.index += 1
+
+         if self.index == len(self.remote):
+             raise StopIteration
+         return self.remote[self.index]
+        
+    
+r = TimeTravelReomote()
+
+itr = iter(r)
+
+print(next(itr))
